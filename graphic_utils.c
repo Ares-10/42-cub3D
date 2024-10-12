a57930bec0ca281319df45d155666fd9c7185fe4
Ares-10 <cholionking1@naver.com>
2024-10-12 18:06:38 +0900

    fix: 렌더링 오류 수정

--- graphic_utils.c
+++ graphic_utils.c
@@ -6,7 +6,7 @@
 /*   By: hyungcho <hyungcho@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/12 01:58:32 by hyungcho          #+#    #+#             */
-/*   Updated: 2024/10/12 02:00:51 by hyungcho         ###   ########.fr       */
+/*   Updated: 2024/10/12 18:06:35 by hyungcho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ void	my_mlx_pixel_put(t_game *game, int x, int y, int color)
 {
 	char	*dst;
 
+	if (x < 0 || x > 1280 || y < 0 || y > 720)
+		return ;
 	dst = game->addr + (y * game->line_length + x * (game->bpp / 8));
 	*(unsigned int *)dst = color;
 }
